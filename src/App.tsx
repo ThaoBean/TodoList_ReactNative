571f6705d9ffcc7640f8864d8db3901cbd740642
thao.do <thaobean1701@gmail.com>
2022-07-06 09:54:34 +0700

    ui welcome page:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import About from './pages/about';
 // import Login from './components/templates/Login';
-import WelcomePage from './pages/welcome';
+// import WelcomePage from './pages/welcome';
 const App = () => {
   return (
     <>
-      <WelcomePage />
+      <About />
     </>
   );
 };
